4e239be851fa3835daa9bde68bd08664b088edc7
Jechol Lee <jechol@devall.org>
2025-05-28 13:44:41 +0900

    Fix style

--- test/ash_default_sort/has_many_sort_test.exs
+++ test/ash_default_sort/has_many_sort_test.exs
@@ -1,68 +1,64 @@
-alias AshDefaultSort.HasManySortTest, as: ThisTest
+defmodule AshDefaultSort.HasManySortTest do
+  use ExUnit.Case, async: true
 
-defmodule ThisTest.TodoList do
-  use Ash.Resource,
-    data_layer: Ash.DataLayer.Ets,
-    domain: AshDefaultSort.TestDomain,
-    extensions: [AshDefaultSort]
+  alias __MODULE__.{TodoList, Task}
 
-  actions do
-    defaults [:read, create: :*]
+  defmodule TodoList do
+    use Ash.Resource,
+      data_layer: Ash.DataLayer.Ets,
+      domain: AshDefaultSort.TestDomain,
+      extensions: [AshDefaultSort]
 
-    update :add_task do
-      require_atomic? false
-      argument :task, :map, allow_nil?: false
-      change manage_relationship(:task, :tasks, type: :create)
+    attributes do
+      uuid_v7_primary_key :id
+      attribute :title, :string, allow_nil?: false, public?: true
+    end
+
+    relationships do
+      has_many :tasks, Task
+    end
+
+    actions do
+      defaults [:read, create: :*]
+
+      update :add_task do
+        require_atomic? false
+        argument :task, :map, allow_nil?: false
+        change manage_relationship(:task, :tasks, type: :create)
+      end
+    end
+
+    default_sort do
+      has_many_sort priority: :asc
     end
   end
 
-  attributes do
-    uuid_v7_primary_key :id
-    attribute :title, :string, allow_nil?: false, public?: true
+  defmodule Task do
+    use Ash.Resource,
+      data_layer: Ash.DataLayer.Ets,
+      domain: AshDefaultSort.TestDomain,
+      extensions: [AshDefaultSort]
+
+    attributes do
+      uuid_v7_primary_key :id
+      attribute :priority, :integer, allow_nil?: false, public?: true
+    end
+
+    relationships do
+      belongs_to :todo_list, TodoList
+    end
+
+    actions do
+      defaults [:read, create: :*]
+    end
+
+    default_sort do
+    end
   end
-
-  relationships do
-    has_many :tasks, ThisTest.Task
-  end
-
-  default_sort do
-    has_many_sort priority: :asc
-  end
-end
-
-defmodule ThisTest.Task do
-  use Ash.Resource,
-    data_layer: Ash.DataLayer.Ets,
-    domain: AshDefaultSort.TestDomain,
-    extensions: [AshDefaultSort]
-
-  actions do
-    defaults [:read, create: :*]
-  end
-
-  attributes do
-    uuid_v7_primary_key :id
-    attribute :priority, :integer, allow_nil?: false, public?: true
-  end
-
-  relationships do
-    belongs_to :todo_list, ThisTest.TodoList
-  end
-
-  default_sort do
-  end
-end
-
-defmodule ThisTest do
-  use ExUnit.Case, async: true
-
-  Process.sleep(1000)
 
   test "pass" do
-    Process.sleep(1000)
-
     params = %{title: "Todo List 1"}
-    list = Ash.Changeset.for_create(ThisTest.TodoList, :create, params) |> Ash.create!()
+    list = Ash.Changeset.for_create(TodoList, :create, params) |> Ash.create!()
 
     for priority <- Enum.concat(1..5//2, 2..5//2) do
       params = %{task: %{priority: priority}}
--- test/ash_default_sort/sort_test.exs
+++ test/ash_default_sort/sort_test.exs
@@ -1,41 +1,36 @@
-alias AshDefaultSort.SortTest, as: ThisTest
-
-defmodule ThisTest.Obj do
-  use Ash.Resource,
-    data_layer: Ash.DataLayer.Ets,
-    domain: AshDefaultSort.TestDomain,
-    extensions: [AshDefaultSort]
-
-  attributes do
-    uuid_v7_primary_key :id
-    attribute :age, :integer, allow_nil?: false, public?: true
-  end
-
-  actions do
-    defaults [:read, create: :*]
-    read :list
-  end
-
-  default_sort do
-    sort age: :desc
-  end
-end
-
-defmodule ThisTest do
+defmodule AshDefaultSort.SortTest do
   use ExUnit.Case, async: true
-  Process.sleep(1000)
+
+  defmodule Obj do
+    use Ash.Resource,
+      data_layer: Ash.DataLayer.Ets,
+      domain: AshDefaultSort.TestDomain,
+      extensions: [AshDefaultSort]
+
+    attributes do
+      uuid_v7_primary_key :id
+      attribute :age, :integer, allow_nil?: false, public?: true
+    end
+
+    actions do
+      defaults [:read, create: :*]
+      read :list
+    end
+
+    default_sort do
+      sort age: :desc
+    end
+  end
 
   test "pass" do
-    Process.sleep(1000)
-
     for age <- Enum.concat(1..5//2, 2..5//2) do
-      Ash.Changeset.for_create(ThisTest.Obj, :create, %{age: age}) |> Ash.create!()
+      Ash.Changeset.for_create(Obj, :create, %{age: age}) |> Ash.create!()
     end
 
     assert [1, 3, 5, 2, 4] =
-             Ash.Query.for_read(ThisTest.Obj, :read) |> Ash.read!() |> Enum.map(& &1.age)
+             Ash.Query.for_read(Obj, :read) |> Ash.read!() |> Enum.map(& &1.age)
 
     assert [5, 4, 3, 2, 1] =
-             Ash.Query.for_read(ThisTest.Obj, :list) |> Ash.read!() |> Enum.map(& &1.age)
+             Ash.Query.for_read(Obj, :list) |> Ash.read!() |> Enum.map(& &1.age)
   end
 end
