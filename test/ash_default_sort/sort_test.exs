d5baa5b165b4df436e06627ff3ed7e1afe63e08b
Minsub Kim <minsub@devall.org>
2025-05-28 09:46:22 +0900

    test default sort

---
+++ test/ash_default_sort/sort_test.exs
@@ -0,0 +1,38 @@
+alias AshDefaultSort.SortTest, as: ThisTest
+
+defmodule ThisTest.Obj do
+  use Ash.Resource,
+    data_layer: Ash.DataLayer.Ets,
+    domain: AshDefaultSort.TestDomain,
+    extensions: [AshDefaultSort]
+
+  actions do
+    defaults [:read, create: :*]
+    read :list
+  end
+
+  attributes do
+    uuid_v7_primary_key :id
+    attribute :age, :integer, allow_nil?: false, public?: true
+  end
+
+  default_sort do
+    sort age: :desc
+  end
+end
+
+defmodule ThisTest do
+  use ExUnit.Case, async: true
+
+  test "pass" do
+    for age <- Enum.concat(1..5//2, 2..5//2) do
+      Ash.Changeset.for_create(ThisTest.Obj, :create, %{age: age}) |> Ash.create!()
+    end
+
+    assert [1, 3, 5, 2, 4] =
+             Ash.Query.for_read(ThisTest.Obj, :read) |> Ash.read!() |> Enum.map(& &1.age)
+
+    assert [5, 4, 3, 2, 1] =
+             Ash.Query.for_read(ThisTest.Obj, :list) |> Ash.read!() |> Enum.map(& &1.age)
+  end
+end
